a6f77008f4c453a83da73908ffa24f4caa85a533
Kelly Robinson <kellylynnrobinson@gmail.com>
2020-08-31 14:25:12 -0500

    added exercises 1_syntax and exercise 2_objects

---
+++ exercises/e1_syntax_notation.R
@@ -0,0 +1,68 @@
+#Exercise 1: syntax and notation
+
+#1) create 3 objects and assign values to them
+
+
+#2) what operators are you not allowed to use in object names?
+
+
+#3) create a vector object with 10 numbers assigned to it
+
+
+#4) what function would you use to find the number of values assigned to a vector object
+
+
+#5) is 'R' case-sensitive language? Y/N?
+
+
+#6) if you type in a command and 'R' returns a '+' sign, then what does that error mean?
+
+
+#7) demonstrate the two ways you can assign a sequence of numbers to a vector object
+
+
+#8) please provide the commands that will show the data type of the following objects:
+
+  a <- 5
+  
+  b <- 7L
+  
+  c <- "giraffe"
+  
+  d <- c(FALSE,TRUE,FALSE)
+  
+#9) create 2 objects and assign each 5 values
+
+  # using these two objects:
+  
+  #9a) perform addition. How does 'R' match up the values between the two vectors?
+  
+  #9b) perform inner and outer multiplication
+  
+  
+#10) create 2 objects, assign one with 4 values and the other with 3 values
+  
+  # using these two objects:
+  
+  #10a) perform addition. How does 'R' match up the values between the two vectors?
+  
+  #10b) 'R' will generate a warning message when you perform this operation. Why is that?
+  
+#11) peform outer multiplication on 2 objects that have equal lenghths (i.e., the same numbe of values assigned to them)
+  
+  #11a) assign the output to a new object
+  
+  #11b) extract the value in the third row and second column
+  
+  
+#12) round the following vector to the nearest 0.1 decimal place
+  
+  lemon <- pi
+  
+#13) find the mean of the following numbers: 0.5, 1.618, 4 (hint: you will need to assign to a vector object first)
+  
+  
+#14) create a die with six sides and sample it with and without replacement. what is the difference between the two methods?
+  
+  
+#15) what is one way you can ask for help with a function within 'R Studio'?
---
+++ exercises/e2_objects_script.R
@@ -0,0 +1,51 @@
+#Exercise #2: objects
+
+# 1) create an object that includes both numbers and character string
+
+
+
+# 2) create a vector object with 15 values. What happens when you set the length to 20?
+
+
+
+#3) create an object that includes a list and set recursive to 'TRUE'.
+
+## 3a) What is the difference if you set recursive to 'FALSE'?
+
+
+
+#4) create a matrix object that has 3 columns and 5 rows. Name the rows and columns.
+
+##4a) Extract the value in the 2nd row and 1st column.
+
+
+
+#5) create an array object that has 3 levels. Each level should have 6 rows and 4 columns. Name the rows and columns.
+
+##5a) Extract the value in the 3rd level, 4th row, and 2nd column.
+
+##5b) If you wanted to extract all the values in the 1st level and 2nd column, what command would you use?
+
+
+#6) What are the differences between a matrix and an array?
+
+
+#7) run the following object and show me the code to what type and class of object it is.
+
+m <- matrix(data=1:15, nrow = 5, ncol = 3,
+            dimnames = list(c("r1","r2","r3","r4","r5"),
+                            c("c1","c2","c3")))
+
+#8) What symbols are you not allowed to use in object names? Why not?
+
+
+#9) create a data frame object with 4 rows and 4 columns. Give the columns the names of your four favorite cartoon charatcters.
+
+##9a) Assign the data in the data frame's third column to a new object.
+
+##9b) Assign the data in the data frames second row to a new object.
+
+#10) Explain the meme on this t-shirt.
+browseURL(url = "https://www.teepublic.com/t-shirt/2305884-programmer-t-shirt-coding-joke")
+
+#11) Assign the formula for a linear regression to a new object.
